3c3ffbab5a68e59f745b08e32f1e2cde27a53c02
Скуратова <71788554+insomniagirl@users.noreply.github.com>
2021-05-25 18:58:31 +0300

    Add files via upload

---
+++ Test2Servlet.java
@@ -0,0 +1,20 @@
+package by.d0mpi.JavaServlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/test2")
+public class Test2Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        getServletContext().getRequestDispatcher("/hello").forward(req, resp);
+        resp.sendRedirect(req.getContextPath() + "/page");
+
+    }
+}
